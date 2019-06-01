17f39be7f4151b68d071a77b7392395e8ef6241e
cr00z <netrebinr@gmail.com>
2019-06-01 16:37:31 +0700

    refactor some functions

--- cinemas.py
+++ cinemas.py
@@ -26,44 +26,52 @@ def fetch_page(url, params=None, proxy=None):
         return None
 
 
+def parse_afisha_movie(film_meta):
+    def get_content(field):
+        try:
+            return field['content']
+        except KeyError:
+            return None
+
+    def get_float(float_candidate):
+        try:
+            return float(float_candidate)
+        except ValueError:
+            return None
+
+    return {
+        'name': get_content(film_meta),
+        'image': get_content(film_meta.findNext('meta', itemprop='image')),
+        'director': get_content(
+            film_meta.findNext('meta', itemprop='director')
+        ),
+        'description': get_content(
+            film_meta.findNext('meta', itemprop='description')
+        ),
+        'afisha_link': film_meta.findNext('a')['href'],
+        'genre': film_meta.findNext('span').string,
+        'short_desc': film_meta.parent.findNext('p').string,
+        'afisha_rating': get_float(
+            film_meta.parent.parent.findNext('div', 'card__actions').div.string
+        )
+    }
+
+
 def parse_afisha_list(raw_html):
     items_skip = 2
     items_limit = 10
-    afisha_soup = BeautifulSoup(raw_html, 'lxml')
-    film_tags = afisha_soup.find_all(
+    film_tags = BeautifulSoup(raw_html, 'lxml').find_all(
         'meta',
         itemprop='name',
         limit=items_skip+items_limit   # find 12 tags
     )[items_skip:]                     # and skip 1st and 2nd
-    afisha_list = []
-    for film_meta in film_tags:
-        afisha_rating = film_meta.parent.parent.findNext('div', 'card__actions').div.string
-        print(afisha_rating)
-        try:
-            afisha_rating = float(afisha_rating)
-        except ValueError:
-            afisha_rating = None
-        film_content = {
-            'name': film_meta['content'],
-            'image': film_meta.findNext('meta', itemprop='image')['content'],
-            'director': film_meta.findNext('meta', itemprop='director')['content'],
-            'description': film_meta.findNext('meta', itemprop='description')['content'],
-            'afisha_link': film_meta.findNext('a')['href'],
-            'genre': film_meta.findNext('span').string,
-            'short_desc': film_meta.parent.findNext('p').string,
-            'afisha_rating': afisha_rating
-        }
-        afisha_list.append(film_content)
-    return afisha_list
+    return [parse_afisha_movie(film_meta) for film_meta in film_tags]
 
 
 def get_afisha_list(cache):
     afisha_list = cache.get('afisha-list')
-    print(afisha_list)
     if afisha_list is None:
-        afisha_list = parse_afisha_list(
-            fetch_page(AFISHA)
-        )
+        afisha_list = parse_afisha_list(fetch_page(AFISHA))
         cache.set('afisha-list', afisha_list, timeout=3600)
     return afisha_list
 
@@ -122,10 +130,10 @@ def parse_kinopoisk_movie_rating(movie_url, proxy):
     movie_rating_str = find_info_in_soup(kp_soup, 'span', {'class': 'rating_ball'})
     if not movie_rating_str:
         movie_votes_str = find_info_in_soup(
-                kp_soup,
-                'span',
-                {'title': 'Рейтинг скрыт (недостаточно оценок)'},
-                next_sibling=True
+            kp_soup,
+            'span',
+            {'title': 'Рейтинг скрыт (недостаточно оценок)'},
+            next_sibling=True
         )
         movie_rating_str = '0'
         if not movie_votes_str:
@@ -149,7 +157,7 @@ def parse_kinopoisk_info(movie_title, proxies_pool, cache):
         movie_title,
         proxies_pool,
     )
-    movie_rating = parse_kinopoisk_info_callback(
+    movie_rates = parse_kinopoisk_info_callback(
         parse_kinopoisk_movie_rating,
         movie_url,
         proxies_pool,
@@ -158,8 +166,8 @@ def parse_kinopoisk_info(movie_title, proxies_pool, cache):
         movie_title,
         {
             'kp_link': movie_url,
-            'kp_rating': movie_rating[0],
-            'kp_votes': movie_rating[1]
+            'kp_rating': movie_rates[0],
+            'kp_votes': movie_rates[1]
         },
         timeout=3600
     )
