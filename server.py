7ceb0ec9e5d68888ed9ab9d691cc9321a689756b
cr00z <netrebinr@gmail.com>
2019-06-09 15:24:28 +0700

    make work correctly again

--- server.py
+++ server.py
@@ -1,19 +1,10 @@
 ﻿from flask import Flask, render_template, url_for
 from werkzeug.contrib.cache import FileSystemCache
 from concurrent.futures import ThreadPoolExecutor
-from threading import active_count
+from threading import Thread, active_count
 import time
 import sys
 import cinemas
-
-
-'''
-Не получается корректно реализовать ThreadPoolExecutor.
-Потоки блокируют основной поток до своего завершения и пользователь не получает вывод.
-Для Thread есть костыль thread.daemon, а тут не получается.
-+ heroku накладывает ограничение на работу тредов в 30 секунд
-Что можно сделать?
-'''
 
 
 SLEEP_DELAY = 25    # for 30 sec heroku limit
@@ -26,17 +17,39 @@ thread_results = []
 
 
 def start_kinopoisk_parser(movies_names):
-    proxies_list = cinemas.get_proxies_list(cache)
-    if proxies_list is None:
-        return None
+    '''
+    Не получается корректно реализовать ThreadPoolExecutor.
+    Потоки блокируют основной поток до своего завершения и пользователь не получает вывод.
+    Для Thread есть костыль thread.daemon, а тут не получается.
+    + heroku накладывает ограничение на работу тредов в 30 секунд
+    Что можно сделать?
+
     with ThreadPoolExecutor() as pool:
         for movie_name in movies_names:
             thread_results.append(pool.submit(
                 cinemas.parse_kinopoisk_info,
                 movie_name,
                 proxies_list,
-                cache 
+                cache
             ))
+    '''
+    proxies_list = cinemas.get_proxies_list(cache)
+    if proxies_list is None:
+        return None
+    for movie_name in movies_names:
+        thread = Thread(
+            target=cinemas.parse_kinopoisk_info,
+            args=(movie_name, proxies_list, cache)
+        )
+        thread.daemon = True
+        thread.start()
+
+
+def sleep_while_threads_works_or_timeout(seconds):
+    for count in range(seconds):
+        if active_count() == 0:
+            break
+        time.sleep(1)
 
 
 def add_kinopoisk_info(movies_list):
@@ -57,6 +70,7 @@ def films_list():
         return render_template('films_list.html', error=True)
     movies_names = [movie['name'] for movie in movies_list]
     start_kinopoisk_parser(movies_names)
+    sleep_while_threads_works_or_timeout(SLEEP_DELAY)
     add_kinopoisk_info(movies_list)
     return render_template('films_list.html', movies_list=movies_list)
 
